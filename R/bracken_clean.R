53986f429e901b0077dd82cdb2595dfb40f3283b
Mike Woodworth <59875549+michaelwoodworth@users.noreply.github.com>
2021-01-07 20:42:18 -0500

    minor updates

--- R/bracken_clean.R
+++ R/bracken_clean.R
@@ -10,20 +10,36 @@
 #' @examples
 #' bracken_clean(data = "bracken_s")
 
-bracken_clean <- function(data, min_abund=0.01){
+bracken_clean <- function(data, min_abund=0.01, frac_or_num="frac"){
+
+  # filter taxa by minimum abundance & trim "_frac" suffix
+  if(frac_or_num=="frac"){
 
     # select just taxonomy fraction and taxon name columns
     Slim_Data <- data %>%
       select(-c(taxonomy_id, taxonomy_lvl)) %>%
       select(-matches("_num"))
 
-    # filter taxa by minimum abundance & trim "_frac" suffix
-    Slim_above_min_abundance <- Slim_Data %>%
+    Slim_step2 <- Slim_Data %>%
       filter_at(vars(ends_with("_frac")), any_vars(. >= min_abund)) %>%  #<----- minimum abundance to adjust
       column_to_rownames(var="name") %>%
       rename_at(.vars = vars(ends_with("_frac")),
                 .funs = funs(sub("_frac$", "", .)))
-
-    # return filtered data frame
-    return(Slim_above_min_abundance)
   }
+
+  # select & trim "_num" suffix
+  if(frac_or_num=="num"){
+
+    Slim_Data <- data %>%
+      select(-c(taxonomy_id, taxonomy_lvl)) %>%
+      select(-matches("_frac"))
+
+    Slim_step2 <- Slim_Data %>%
+      column_to_rownames(var="name") %>%
+      rename_at(.vars = vars(ends_with("_num")),
+                .funs = funs(sub("_num$", "", .)))
+  }
+
+  # return filtered data frame
+  return(Slim_step2)
+}
---
+++ R/get_bracken_taxa.R
@@ -0,0 +1,21 @@
+#' Get Bracken Taxa
+#'
+#' This function accepts a list of bracken summary files
+#' and returns a taxonomy table for use in phyloseq analyses.
+#'
+#' @param data bracken summary table or list of tables
+#' @param level bracken taxonomic level or list of levels matching data objects
+#' @keywords MEP
+#' @export
+#' @examples
+#' get_bracken_taxa(data = c(bracken_s,bracken_g,bracken_f), level = c("Species", "Genus", "Family"))
+
+get_bracken_taxa <- function(data, level){
+
+  # loop over length of list of data objects / levels
+  for(i in )
+
+
+  # return filtered data frame
+  return(taxa_table)
+}
